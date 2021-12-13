1b3c38c80c25d7a28b52c784257b47df32b5c4ee
Brandie Parker <brandie.parker27@gmail.com>
2021-12-12 22:53:48 -0500

    updated py and html files

--- Mission_to_Mars/app.py
+++ Mission_to_Mars/app.py
@@ -1,27 +1,27 @@
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
-import mission_to_mars
+import scrape_mars
 
 # Create an instance of our Flask app.
 app = Flask(__name__)
 
 # Use flask_pymongo to set up mongo connection
-app.config["MONGO_URI"] = "mongodb://localhost:27017/phone_app"
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
 
-db.mars.drop()
+# db.mars.drop()
 
 # Set route
 @app.route('/')
 def index():
-    data= mongo.db.marsdb.find_one()    
-    return render_template("index.html", marsdb=data)
+    mars_data= mongo.db.mars_data.find_one()    
+    return render_template("index.html", mars_data=mars_data)
 
 @app.route("/scrape")
 def scraper():
-    marsdb = mongo.db.marsdb
+    mars_data = mongo.db.mars_data
     mars_data = scrape_mars.scrape()
-    marsdb.update_many({}, {"$set": mars_data}, upsert=True)
+    mars_data.replace_one({mars_data}, mars_data, upsert=True)
     return redirect("/", code=302)
 
 
--- Mission_to_Mars/scrape_mars.py
+++ Mission_to_Mars/scrape_mars.py
@@ -6,7 +6,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 import os
 import time
 
-mars_data = {}
+data = {}
 
 
 def scrape():
@@ -23,8 +23,8 @@ def scrape():
     news_p = article_soup.find('div', class_='article_teaser_body').text
 
     #insert data into dictionary
-    mars_data['news_title'] = news_title
-    mars_data['news_p'] = news_p
+    data['news_title'] = news_title
+    data['news_p'] = news_p
 
     # browser.quit()
     # return mars_data
@@ -37,7 +37,7 @@ def scrape():
     img_soup = BeautifulSoup(html_two, 'html.parser')
     feature_image = img_soup.find('img', class_='headerimage').get("src")
     feature_img_url= url_two + feature_image
-    mars_data['feature_img_url'] = feature_img_url
+    data['feature_img_url'] = feature_img_url
 
     # browser.quit()
     # return mars_data
@@ -50,7 +50,7 @@ def scrape():
     # facts_soup = BeautifulSoup(html_three, 'html.parser')
     mars_facts_df = ((pd.read_html(url_three))[0]).rename(columns={0: "Attribute", 1: "Mars", 2:"Earth"}).set_index(['Attribute'])
     fact_html_table = (mars_facts_df.to_html()).replace('\n', '')
-    mars_data['fact_html_table'] = fact_html_table
+    data['fact_html_table'] = fact_html_table
 
     # scrape hemisphere data
     hemi1_url = 'https://marshemispheres.com/cerberus.html'
@@ -61,8 +61,8 @@ def scrape():
     cerberus_title = hemi1_soup.find('h2', class_='title').text
     cerberus_img = hemi1_soup.find('img', class_='wide-image').get('src')
     cerberus_url = "https://marshemispheres.com/" + cerberus_img
-    mars_data['cerberus_title'] = cerberus_title
-    mars_data['cerberus_url'] = cerberus_url
+    data['cerberus_title'] = cerberus_title
+    data['cerberus_url'] = cerberus_url
 
     hemi2_url = 'https://marshemispheres.com/schiaparelli.html'
     browser.visit(hemi2_url)
@@ -72,8 +72,8 @@ def scrape():
     schiaparelli_title = hemi2_soup.find('h2', class_='title').text
     schiaparelli_img = hemi2_soup.find('img', class_='wide-image').get('src')
     schiaparelli_url = "https://marshemispheres.com/" + schiaparelli_img
-    mars_data['schiaparelli_title'] = schiaparelli_title
-    mars_data['schiaparelli_url'] = schiaparelli_url
+    data['schiaparelli_title'] = schiaparelli_title
+    data['schiaparelli_url'] = schiaparelli_url
 
     hemi3_url = 'https://marshemispheres.com/syrtis.html'
     browser.visit(hemi3_url)
@@ -83,8 +83,8 @@ def scrape():
     syrtis_major_title = hemi3_soup.find('h2', class_='title').text
     syrtis_major_img = hemi3_soup.find('img', class_='wide-image').get('src')
     syrtis_major_url = "https://marshemispheres.com/" + syrtis_major_img
-    mars_data['syrtis_major_title'] = syrtis_major_title
-    mars_data['syrtis_major_url'] = syrtis_major_url
+    data['syrtis_major_title'] = syrtis_major_title
+    data['syrtis_major_url'] = syrtis_major_url
 
 
     hemi4_url = 'https://marshemispheres.com/valles.html'
@@ -95,11 +95,13 @@ def scrape():
     valles_marineris_title = hemi4_soup.find('h2', class_='title').text
     valles_marineris_img = hemi4_soup.find('img', class_='wide-image').get('src')
     valles_marineris_url = "https://marshemispheres.com/" + valles_marineris_img
-    mars_data['valles_marineris_title'] = valles_marineris_title
-    mars_data['valles_marineris_url'] = valles_marineris_url
+    data['valles_marineris_title'] = valles_marineris_title
+    data['valles_marineris_url'] = valles_marineris_url
 
     # or do i need to append the hemisphere data to a list and then add that list to the mars_data?
-    return mars_data
+    
+    browser.quit()
+    return data
 
 
 
